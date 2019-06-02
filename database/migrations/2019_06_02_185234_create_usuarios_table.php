06af12b84dfe67a1be7a1dd3e8ee7655460ecdbe
Magnos <magandroid6.1@gmail.com>
2019-06-02 17:32:12 -0300

    Commit inicial, com migrações de tabelas logs e usuarios implementadas

---
+++ database/migrations/2019_06_02_185234_create_usuarios_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('login', 50);
+            $table->string('senha', 60);
+            $table->string('nome', 200);
+            $table->string('cpf', 11);
+            $table->string('tipo_acesso', 3);
+            $table->boolean('ativo', 1);
+            $table->string('token', 255)->nullable();
+            $table->timestamps();
+            $table->timestamp('last_access')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
---
+++ database/migrations/2019_06_02_194915_create_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('acao', 500);
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')
+                    ->references('id')
+                    ->on('usuarios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}

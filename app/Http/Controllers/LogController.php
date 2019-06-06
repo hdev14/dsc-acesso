e24fc04ec6909da67f4c6250c755c024cf553498
Magnos <magandroid6.1@gmail.com>
2019-06-05 23:39:09 -0300

    api recebendo acao de log e salvando

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -9,8 +9,8 @@ class LogController extends Controller
 {
 	public function criar(Request $req) {
 		//Implementar validação
-        $log = $req->json();
-        $log->usuario_id = 1;
+        $log = new Log($req->json()->all());
+        //Pegar id de usuario
         $log->save();
         return response()->json(['msg' => 'Log adicionado com sucesso'], 200);
     }
